7a74d91c74e632747376af1e415f0a4812029768
Gustavo Martínez <gustavomartinez1990@gmail.com>
2022-10-24 10:55:01 -0400

    feat(learn): add property binding and string interpolation examples

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,20 @@
 import { Component } from '@angular/core';
 
+const name = 'Gustavo';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'my-store';
+  name = name;
+  age = 32;
+  image = 'https://source.unsplash.com/random';
+  buttonDisabled = true;
+  person = {
+    name: 'Gustavo Martinez',
+    age: 32,
+    avatar: 'https://source.unsplash.com/random'
+  };
 }
