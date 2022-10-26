49484a266ab52c9d5d2496587cacaa4f4bfd4e87
Gustavo Martínez <gustavomartinez1990@gmail.com>
2022-10-25 23:02:10 -0400

    feat(learn): add ngFor examples

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Product } from './product.model';
 
 const name = 'Gustavo';
 
@@ -17,6 +18,26 @@ export class AppComponent {
     age: 32,
     avatar: 'https://source.unsplash.com/random'
   };
+
+  emojis: string[] = [ '😂' , '🐦', '🐳', '🌮', '💚'];
+
+  names: string[] = [ 'Gustao'];
+
+  newName = '';
+
+  addName() {
+    this.names.push(this.newName);
+    this.newName = "";
+  }
+
+  deleteName() {
+    const index = this.names.indexOf(this.newName);
+    this.names.splice(index, 1);
+  }
+
+  deleteNameParameter(index: number) {
+    this.names.splice(index, 1);
+  }
 
   public toggleButton() {
     console.log('The form was sent');
@@ -38,4 +59,38 @@ export class AppComponent {
     this.person.name = element.value;
     console.log(element.value);
   }
+
+  products: Product[]  = [
+    {
+      name: 'EL mejor juguete',
+      price: 565,
+      image: './assets/images/toy.jpg',
+      category: 'all',
+    },
+    {
+      name: 'Bicicleta casi nueva',
+      price: 356,
+      image: './assets/images/bike.jpg',
+    },
+    {
+      name: 'Colleción de albumnes',
+      price: 34,
+      image: './assets/images/album.jpg'
+    },
+    {
+      name: 'Mis libros',
+      price: 23,
+      image: './assets/images/books.jpg'
+    },
+    {
+      name: 'Casa para perro',
+      price: 34,
+      image: './assets/images/house.jpg'
+    },
+    {
+      name: 'Gafas',
+      price: 3434,
+      image: './assets/images/glasses.jpg'
+    }
+  ]
 }
